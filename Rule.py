f375b41fb039c1ba00a9a9669926c74dee23960b
Anna Glid <anna.glid27@gmail.com>
2022-02-15 13:27:08 +0100

    Delete class Rule, add methods in the module Rule

--- Rule.py
+++ Rule.py
@@ -1,15 +1,35 @@
 
-class Rule:
-    def __init__(self, attributes, decision):
-        self.attributes = attributes
-        self.decision = decision
+def is_rule_true(rule_a, rule_b):
+    if __decision(rule_a) != __decision(rule_b):
+        return False
+    if __rule_length(rule_a) < __rule_length(rule_b):
+        return False
+    if __attributes_set(rule_b).issubset(__attributes_set(rule_a)):
+        return True
+    else:
+        return False
 
-    def __str__(self):
-        string_rule = ''
-        for (attribute, value) in self.attributes.items():
-            string_rule += '{}={} '.format(attribute, value)
-        string_rule += 'd={}'.format(self.decision)
-        return string_rule
+def __decision(rule):
+    # decision is at the end of a rule
+    return rule[-1]
+
+def __rule_length(rule):
+    # return the number of attributes in a rule
+    # minus one because of the decision
+    return rule.count('=') - 1 
     
-    def append_attribute(self, attribute, value):
-        pass
+def __attributes_set(rule):
+    rule = rule[:rule.find('d')-1]  
+    # minus one because of the space before decision
+    return set(rule.split(' '))
+
+def choose_shortest_rules(rules_list):
+    min_length = 100
+    result = []
+    for rule in rules_list:
+        if __rule_length(rule) < min_length:
+            result.clear()
+            min_length = __rule_length(rule)
+        if __rule_length(rule) == min_length:
+            result.append(rule)
+    return result
